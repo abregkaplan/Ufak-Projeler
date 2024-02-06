f1e083cce4c34d3939b282e51f7c882675b69cba
Abreg <muhammed_abreg@hotmail.com>
2024-02-06 18:09:15 +0300

    Sınav Notu Hesaplama projesi eklendi

---
+++ Sinav_Notu_Hesaplama/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sinav_Notu_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double s1, s2, proje, ort;
+            string ad, soyad;
+            ad = textBox1.Text;
+            soyad = textBox2.Text;
+            s1 = Convert.ToDouble(textBox3.Text);
+            s2 = Convert.ToDouble(textBox4.Text);
+            proje = Convert.ToDouble(textBox5.Text);
+            ort = (s1 + s2 + proje) / 3;
+            listBox1.Items.Add("Adı:" + ad +" "+ soyad + " 1.Sınav: " + s1 + " 2.Sınav: " + s2 + " Proje Notu: " + proje);
+        }
+    }
+}
